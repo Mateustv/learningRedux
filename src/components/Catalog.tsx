9f162f29595a624cebcd18cf4e07c3ef04336d5d
Mateus T <mateustavarestv@hotmail.com>
2022-03-08 18:37:11 -0300

    adicionado item no carinho por meio da estado

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
 import api from "../services/api"
+import { addProductToCart } from "../store/modules/cart/actions"
 import { IProduct } from "../store/modules/cart/types"
 
 export default function Catalog() {
 
+  const dispatch = useDispatch()
   const [catalog, setCatalog] = useState<IProduct[]>([])
 
   useEffect(() => {
@@ -12,17 +15,27 @@ export default function Catalog() {
     })
   }, [])
 
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product))
+  }, [])
   return (
     <div>
       <h1>Catalog</h1>
       {catalog.map(data => (
         <article key={data.id}>
           <strong>{data.title}</strong>
-          <strong>       : {data.price} $</strong>{"  "}
+          <strong>       : {data.price} $</strong>{"   "}
 
-          <button type="button">Comprar</button>
+          <button
+            type="button"
+            onClick={() => handleAddProductToCart(data)}
+          >
+            Comprar
+          </button>
         </article>
       ))}
     </div>
   )
 }
+
+
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,11 @@
 import { createStore } from 'redux'
+import { ICartState } from './modules/cart/types'
 import rootReducer from './modules/rootReducer'
 
 const store = createStore(rootReducer)
+
+export interface IState {
+  cart: ICartState
+}
 
 export default store
